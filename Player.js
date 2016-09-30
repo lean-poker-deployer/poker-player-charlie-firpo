ba6b86ffad93407f3d09393c4b221722107d67a5
devill <devillmeister@gmail.com>
2016-09-30 11:34:05 +0200

    Don't risk at all

--- Player.js
+++ Player.js
@@ -10,12 +10,12 @@ class Player {
     if(hole_cards[0].rank === hole_cards[1].rank && [9,10,"J","Q","K","A"].indexOf(hole_cards[1].rank) >= 0) {
       return 10000;
     } else {
-      console.log("SMALL BLIND: " + gameState.small_blind + " BUY IN:" + gameState.current_buy_in);
-      if (gameState.current_buy_in < gameState.small_blind * 4) {
-        return Math.max(0, gameState.current_buy_in - me.bet + gameState.minimum_raise);
-      } else {
-        return 0; 
-      }
+      return 0;
+      // if (gameState.current_buy_in < gameState.small_blind * 4) {
+      //   return Math.max(0, gameState.current_buy_in - me.bet + gameState.minimum_raise);
+      // } else {
+      //   return 0;
+      // }
     }
   }
 
