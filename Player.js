e80a6ed7a6e5bb0c22182512b3aeb551bb4d0e44
DeVill <devillmeister@gmail.com>
2016-09-30 10:24:43 +0200

    Update Player.js

--- Player.js
+++ Player.js
@@ -4,7 +4,17 @@ class Player {
   }
 
   static betRequest(gameState) {
-    return 10000;
+    var me = gameState.players[gameState.in_action];
+    var hole_cards = me.hole_cards;
+    if(hole_cards[0].rank === hole_cards[1].rank) {
+      return 10000;
+    } else {
+      if (gameState.current_buy_in < gameState.small_blind * 4) {
+        return gameState.current_buy_in - me.bet _ gameState.minimum_raise;
+      } else {
+        return 0; 
+      }
+    }
   }
 
   static showdown(gameState) {
